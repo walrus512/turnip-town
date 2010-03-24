e02eb136760a0dbbe98f3169164f39a0e4343081
11y3y3y3y43 <11y3y3y3y43@072821e6-c1ea-11de-bbac-836dcc338a7a>
2010-03-24 02:02:58 +0000

    Added download_feed.py to main_utils, grabs rss feeds and spits back the results. Added RSS sifting to the List Sifter tab, it stores, grabs, and allows you to sift RSS feeds for songs. Added grooveshark version number editing ability to Help menu. Added 'post' option to List Sifter to allow you to remove extra crap that occurs after the song on a text list. Added [#]char to pre-sift field so you can specify where to start chopping the text eg. '[2].' is 2nd period. Fixed the sending of version to the gs api.

--- trunk/groovewalrus/gw.py
+++ trunk/groovewalrus/gw.py
@@ -47,6 +47,7 @@ from main_utils import plugin_loader
 from main_utils import system_files
 from main_utils import file_cache
 from main_utils import prefetch
+from main_utils import download_feed
 
 from main_controls import drag_and_drop
 from main_controls import playback_panel
@@ -75,7 +76,7 @@ from main_thirdp import grooveshark_old
 #from plugins.griddle import griddle
 #from plugins.ratings import ratings
 
-PROGRAM_VERSION = "0.209"
+PROGRAM_VERSION = "0.210"
 PROGRAM_NAME = "GrooveWalrus"
 PLAY_SONG_URL ="http://listen.grooveshark.com/songWidget.swf?hostname=cowbell.grooveshark.com&style=metal&p=1&songID="
 PLAY_SONG_ALTERNATE_URL ="http://listen.grooveshark.com/main.swf?hostname=cowbell.grooveshark.com&p=1&songID="
@@ -266,6 +267,10 @@ class MainPanel(wx.Panel):
         # sifter
         self.tc_sift_pre = xrc.XRCCTRL(self, 'm_tc_sift_pre')
         self.tc_sift_seperator = xrc.XRCCTRL(self, 'm_tc_sift_seperator')
+        self.tc_sift_post = xrc.XRCCTRL(self, 'm_tc_sift_post')
+        self.lc_sift_rss = xrc.XRCCTRL(self, 'm_lc_sift_rss')
+        self.tc_sift_rss_url = xrc.XRCCTRL(self, 'm_tc_sift_rss_url')
+        self.ch_sift_rss = xrc.XRCCTRL(self, 'm_ch_sift_rss')
         self.tc_sift_rawlist = xrc.XRCCTRL(self, 'm_tc_sift_rawlist')
         
         # bio
@@ -409,18 +414,26 @@ class MainPanel(wx.Panel):
         for file_name in image_files:
             bmp = wx.Bitmap(file_name, wx.BITMAP_TYPE_PNG)
             rate_images.Add(bmp)            
-        self.lc_faves.AssignImageList(rate_images, wx.IMAGE_LIST_SMALL)        
-                
+        self.lc_faves.AssignImageList(rate_images, wx.IMAGE_LIST_SMALL)
+        
+        # list sifter: list control -------
         self.lc_sift = xrc.XRCCTRL(self, 'm_lc_sift')
         self.lc_sift.InsertColumn(0,"Artist")
         self.lc_sift.InsertColumn(1,"Song")
-        #self.lc_faves.InsertColumn(2,"Album")
-        #self.lc_faves.InsertColumn(3,"Id")
         self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.OnSiftDoubleClick, self.lc_sift)
         # wxMSW
         self.Bind(wx.EVT_COMMAND_RIGHT_CLICK, self.OnSiftRightClick, self.lc_sift)
         # wxGTK
         self.lc_sift.Bind(wx.EVT_COMMAND_RIGHT_CLICK, self.OnSiftRightClick)
+        
+        # list sifter: rss list control ----
+        self.lc_sift_rss = xrc.XRCCTRL(self, 'm_lc_sift_rss')
+        self.lc_sift_rss.InsertColumn(0,"Feed Url")
+        self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnRSSClick, self.lc_sift_rss)
+        # wxMSW
+        self.Bind(wx.EVT_COMMAND_RIGHT_CLICK, self.OnRSSRightClick, self.lc_sift_rss)
+        # wxGTK
+        self.lc_sift_rss.Bind(wx.EVT_COMMAND_RIGHT_CLICK, self.OnRSSRightClick)
         
         # ---------------------------------------------------------------
                 
@@ -512,14 +525,20 @@ class MainPanel(wx.Panel):
         self.Bind(wx.EVT_BUTTON, self.OnAlbumSearchClick, id=xrc.XRCID('m_bb_album_search'))
         self.bm_cover_large.Bind(wx.EVT_LEFT_UP, self.OnAlbumCoverClick)
         self.bm_cover.Bind(wx.EVT_LEFT_UP, self.OnAlbumCoverClick)
-        self.Bind(wx.EVT_BUTTON, self.OnUpdateClick, self.bb_update)
-                
-        self.Bind(wx.EVT_BUTTON, self.OnSiftClick, id=xrc.XRCID('m_bu_sift_sift'))
+        self.Bind(wx.EVT_BUTTON, self.OnUpdateClick, self.bb_update)        
         
+        #list sifter
+        self.Bind(wx.EVT_BUTTON, self.OnSiftClick, id=xrc.XRCID('m_bu_sift_sift'))
+        self.Bind(wx.EVT_BUTTON, self.OnSiftRSS, id=xrc.XRCID('m_bu_sift_rss'))
+        self.Bind(wx.EVT_BUTTON, self.OnSaveRSSClick, id=xrc.XRCID('m_bb_sift_rss_save'))
+        self.Bind(wx.EVT_BUTTON, self.OnSiftURLClear, id=xrc.XRCID('m_bb_sift_url_clear'))
+        
+        #my last.fm
         self.Bind(wx.EVT_BUTTON, self.OnMyLastClearClick, id=xrc.XRCID('m_bb_mylast_clear'))
         self.Bind(wx.EVT_BUTTON, self.OnMyLastSearchClick, id=xrc.XRCID('m_bb_mylast_search'))
         self.Bind(wx.EVT_BUTTON, self.OnMyLastWebClick, id=xrc.XRCID('m_bb_mylast_goweb'))
-                
+        
+        #options
         self.Bind(wx.EVT_RADIOBOX, self.SaveOptions, id=xrc.XRCID('m_rx_options_double_click'))
         self.Bind(wx.EVT_RADIOBOX, self.SaveOptions, id=xrc.XRCID('m_rx_options_scrobble_port'))
         self.Bind(wx.EVT_SPINCTRL, self.SaveOptions, id=xrc.XRCID('m_sc_options_gs_wait'))
@@ -790,7 +809,8 @@ class MainPanel(wx.Panel):
         self.parent.Bind(wx.EVT_MENU, self.OnToggleScrobble, id=7666)        
         
         # help menu
-        self.parent.Bind(wx.EVT_MENU, self.OnAboutClick, id=xrc.XRCID("m_mi_about"))        
+        self.parent.Bind(wx.EVT_MENU, self.OnGSVersionClick, id=xrc.XRCID("m_mi_gs_version"))
+        self.parent.Bind(wx.EVT_MENU, self.OnAboutClick, id=xrc.XRCID("m_mi_about"))
         
         
         #-------------
@@ -806,6 +826,9 @@ class MainPanel(wx.Panel):
         # clean cache dir -------
         temp_dir = system_files.GetDirectories(self).TempDirectory()
         file_cache.CheckCache(temp_dir)
+        
+        # load rss feeds --------
+        self.LoadRSSFeeds()
         
         #print system_files.GetDirectories(self).TempDirectory()
         #print system_files.GetDirectories(self).DataDirectory()
@@ -1130,6 +1153,27 @@ class MainPanel(wx.Panel):
             
     def OnAboutClick(self, event):
         options_window.Options(self).ShowAbout(PROGRAM_NAME, PROGRAM_VERSION)
+        
+    def OnGSVersionClick(self, event):
+        #show a dialog tat let's you change the GS Version
+        dlg = wx.TextEntryDialog(self, 'Grooveshark version:', 'Grooveshark Version')
+        data_dir = system_files.GetDirectories(self).DataDirectory() + os.sep
+        g_version = GetLocalGroovesharkVersion(data_dir)
+        if g_version == None:
+            g_version = ''
+        dlg.SetValue(g_version)
+
+        if dlg.ShowModal() == wx.ID_OK:
+            if g_version != dlg.GetValue():
+                #save xml file
+                file_name = 'grooveshark.xml'    
+                if dlg.GetValue() !='':
+                    #get the version number from the file
+                    print 'asfasfasf'
+                    data_dict = {'version':dlg.GetValue()}                    
+                    read_write_xml.xml_utils().save_generic_settings(data_dir, file_name, data_dict)
+        dlg.Destroy()
+        
            
     def SetVolume(self, volume):
         soundmixer.SetMasterVolume(volume)
@@ -3051,6 +3095,7 @@ class MainPanel(wx.Panel):
         # add the results to the list control
         sift_pre = self.tc_sift_pre.GetValue()
         sift_seperator = self.tc_sift_seperator.GetValue()
+        sift_post = self.tc_sift_post.GetValue()
         s_choice = xrc.XRCCTRL(self, 'm_ch_sift_choice').GetSelection()
         #print s_choice
         # check if something is selected
@@ -3060,34 +3105,122 @@ class MainPanel(wx.Panel):
         self.lc_sift.DeleteAllItems()
         counter = 0;
         
+        # use '[#]char' to skip to the seperator ex [2]. is second .
+        split_number = 1
+        if len(sift_pre) > 3:
+            if (sift_pre[0] == '[') & (sift_pre[2] == ']') & (sift_pre[1].isdigit()):
+                print 'boo'
+                split_number = int(sift_pre[1])
+                sift_pre = sift_pre[3:]
+        
         if (len(text_glob) > 0) & (sift_seperator != ''):
             #print len(text_glob)
             for x in text_glob:
                 if len(sift_pre) > 0:
-                    part_one = x.split(sift_pre, 1)[1]
+                    part_one = x.split(sift_pre, split_number)[-1]
                 else:
                     part_one = x
                 part_two = part_one.split(sift_seperator, 1)
                 artist = part_two[0]
                 if len(part_two) > 1:
                     song = part_two[1]
+                    if sift_post != '':
+                        post_song = song.rsplit(sift_post, 1)
+                        song = post_song[0]
                 else:
-                    song = ''    
+                    song = ''
+                                    
                 # add to list control
                 if s_choice == 0:
                     if (artist != '') & (song != ''):
-                        self.lc_sift.InsertStringItem(counter, artist)
-                        self.lc_sift.SetStringItem(counter, 1, song)
+                        self.lc_sift.InsertStringItem(counter, artist.strip())
+                        self.lc_sift.SetStringItem(counter, 1, song.strip().replace('"', ''))
                         counter = counter + 1
                 else:
                     if (artist != '') & (song != ''):
-                        self.lc_sift.InsertStringItem(counter, song)
-                        self.lc_sift.SetStringItem(counter, 1, artist)          
+                        self.lc_sift.InsertStringItem(counter, song.strip().replace('"', ''))
+                        self.lc_sift.SetStringItem(counter, 1, artist.strip())          
                         counter = counter + 1
                
             self.lc_sift.SetColumnWidth(0, 120)
             self.lc_sift.SetColumnWidth(1, wx.LIST_AUTOSIZE)
             #print self.lc_sift.GetItemCount()
+            
+    def OnRSSClick(self, event):
+        # load selected rss feed
+        val = self.lc_sift_rss.GetFirstSelected()
+        if val != None:
+            feed = self.lc_sift_rss.GetItem(val, 0).GetText()
+            self.tc_sift_rss_url.SetValue(feed)
+            
+    def OnRSSRightClick(self, event):
+        # show menu for deleting the rss feed
+        val = self.lc_sift_rss.GetFirstSelected()
+        if val != -1:
+            if (self.lc_sift.GetItem(val, 0).GetText() != '') & (self.lc_sift.GetItem(val, 1).GetText() != ''):    
+                # make a menu
+                ID_DELETE = 1
+                menu = wx.Menu()
+                menu.Append(ID_DELETE, "Delete Feed")
+                wx.EVT_MENU(self, ID_DELETE, self.OnDeleteRSSClick)       
+                self.PopupMenu(menu)
+                menu.Destroy()
+                
+    def OnDeleteRSSClick(self, event):
+        # remove the rss feed from the database
+        val = self.lc_sift_rss.GetFirstSelected()
+        feed = self.lc_sift_rss.GetItem(val, 0).GetText()
+        local_songs.DbFuncs().RemoveFeedRow(feed)   
+        self.LoadRSSFeeds()
+        
+    def OnSaveRSSClick(self, event):
+        # save the rss feed to the database
+        feed = self.tc_sift_rss_url.GetValue()
+        if feed !='':
+            self.SaveRSSFeed(feed)            
+        
+    def SaveRSSFeed(self, feed_name):
+        #save a feed url to the db               
+        local_songs.DbFuncs().InsertFeedData(feed_name)
+        self.LoadRSSFeeds()
+        
+    def LoadRSSFeeds(self):
+        self.lc_sift_rss.DeleteAllItems()
+        #populate rss listctrl
+        FILEDB = system_files.GetDirectories(None).DatabaseLocation()
+        conn = sqlite3.connect(FILEDB)
+        c = conn.cursor()
+        query = "SELECT feed_url FROM m_feeds ORDER BY feed_url"
+        
+        c.execute(query)
+        h = c.fetchall()
+        counter = 0
+        for x in h:
+            #print x
+            try:
+                index = self.lc_sift_rss.InsertStringItem(counter, x[counter])               
+            except TypeError, err:
+                print 'Type error: ' + str(err)
+                pass
+        c.close()
+        self.lc_sift_rss.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+        
+    def OnSiftRSS(self, event):
+        # load the rss file and pass to the list sifter
+        if (self.tc_sift_rss_url.GetValue() != ''):
+            #fetch rss feed
+            rss_feed = download_feed.GetRSSFeed(self.tc_sift_rss_url.GetValue())
+            #pass to pastebox
+            feed_out = ''
+            for x in rss_feed:
+                feed_out = feed_out + x + '\n'
+            self.tc_sift_rawlist.SetValue(feed_out)
+            #switch to the textbox tab
+            xrc.XRCCTRL(self, 'm_nb_sift_rss').SetSelection(0)
+            
+    def OnSiftURLClear(self, event):
+        #clear the url field
+        self.tc_sift_rss_url.SetValue('')
             
     def OnAutoGenerateSiftPlayist(self, event):
         # copy the sifted list to the playlist
@@ -3300,24 +3433,12 @@ class FileThread(Thread):
         # file size
         keyandserver = self.GetStreamKeyAndServer()
         file_size = grooveshark_old.Grooveshark(self).GetFileSize(keyandserver[0], keyandserver[1])
-        return file_size
-        
-    def GetLocalGroovesharkVersion(self):
-        #gets the grooveshark version from the local grooveshark.xml file
-        #check if file exists
-        file_name = 'grooveshark.xml'
-        g_version = None
-        if os.path.isfile(file_name):
-            #get the version number from the file
-            xml_dict = read_write_xml.xml_utils().get_generic_settings(file_name)
-            if xml_dict.has_key('version'):
-                if len(xml_dict['version']) > 0:
-                    g_version = xml_dict['version']
-        return g_version
+        return file_size        
         
     def GetStreamKeyAndServer(self):
-        g_version = self.GetLocalGroovesharkVersion()
-        g_session = jsonrpcSession(g_version)
+        data_dir = system_files.GetDirectories(self).DataDirectory() + os.sep
+        g_version = GetLocalGroovesharkVersion(data_dir)
+        g_session = jsonrpcSession(None, g_version)
         g_session.startSession()
         g_data = {'SongID': self.song_id, 'Name': self.track, 'ArtistName': self.artist, 'AlbumName': self.album, 'AlbumID': '', 'ArtistID': ''}
         g_song = song.songFromData(g_session, g_data)
@@ -3357,7 +3478,20 @@ class FileThread(Thread):
                 # ASSUME that it's the current selection
                 val = self.parent.lc_playlist.GetFirstSelected()
                 self.parent.lc_playlist.SetStringItem(val, 3, '')
+          
                 
+def GetLocalGroovesharkVersion(data_dir):
+    #gets the grooveshark version from the local grooveshark.xml file
+    #check if file exists
+    file_name = 'grooveshark.xml'
+    g_version = None
+    if os.path.isfile(file_name):
+        #get the version number from the file
+        xml_dict = read_write_xml.xml_utils().get_generic_settings(data_dir + file_name)
+        if xml_dict.has_key('version'):
+            if len(xml_dict['version']) > 0:
+                g_version = xml_dict['version']
+    return g_version     
             
 # ####################################
 class ProgressThread(Thread): 
