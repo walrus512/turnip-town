c52d0125be577618bf591080269bab17dadca7bd
11y3y3y3y43 <11y3y3y3y43@072821e6-c1ea-11de-bbac-836dcc338a7a>
2010-11-02 03:03:57 +0000

    Added Spanish, Dutch, Canadian to the translations, thanks translators. Added 'Select Language' to the tools menu, lets you select a language, you must restart for it to take effect. Updated GS version.

--- trunk/groovewalrus/setup_win3.py
+++ trunk/groovewalrus/setup_win3.py
@@ -19,7 +19,7 @@ class Target:
     def __init__(self, **kw):
         self.__dict__.update(kw)
         # for the versioninfo resources
-        self.version = "0.323"
+        self.version = "0.324"
         self.company_name = "Turnip-Town.net"
         self.copyright = "lots of copyright"
         self.name = "GrooveWalrus"
