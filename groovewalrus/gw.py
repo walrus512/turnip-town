cb44edb4a48826baa2ef066283aab459b4451d5e
11y3y3y3y43@gmail.com <11y3y3y3y43@gmail.com@072821e6-c1ea-11de-bbac-836dcc338a7a>
2011-02-06 01:51:17 +0000

    Updated to a new eyeD3 version, fixed problems with python 2.7.

--- groovewalrus/gw.py
+++ groovewalrus/gw.py
@@ -112,7 +112,7 @@ from main_thirdp import grooveshark_old
 #from plugins.zongdora import zongdora
 #from plugins.web_remote import web_remote
 
-PROGRAM_VERSION = "0.331"
+PROGRAM_VERSION = "0.332"
 PROGRAM_NAME = "GrooveWalrus"
 
 #PLAY_SONG_URL ="http://listen.grooveshark.com/songWidget.swf?hostname=cowbell.grooveshark.com&style=metal&p=1&songID="
