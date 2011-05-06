3aded95a540c72cf9e8fca057ca87e686d8f58fb
11y3y3y3y43@gmail.com <11y3y3y3y43@gmail.com@072821e6-c1ea-11de-bbac-836dcc338a7a>
2011-05-06 04:38:53 +0000

    Added Hotkeys plug-in. Updated a bunch of the menu shortcuts.

--- trunk/groovewalrus/main_utils/global_hotkeys.py
+++ trunk/groovewalrus/main_utils/global_hotkeys.py
@@ -32,7 +32,7 @@ class GlobalHotkeys():
         # defaults
         # prev / play(pause) / stop / next
         self.GetSavedHotkeys()
-        parent.Bind(wx.EVT_HOTKEY, self.SetGlobalHotkey, id=100, id2=103)
+        parent.Bind(wx.EVT_HOTKEY, self.SetGlobalHotkey, id=100, id2=108)
 
     def SetGlobalHotkey(self, event):
         keyp = event.GetId()
@@ -40,10 +40,20 @@ class GlobalHotkeys():
             self.parent.OnBackwardClick(event=None)
         elif keyp == 101:
             self.parent.OnPlayClick(event=None)
+        elif keyp == 102:
+            self.parent.OnStopClick(event=None)
         elif keyp == 103:
             self.parent.OnForwardClick(event=None)
-        else:
-            self.parent.OnStopClick(event=None)
+        elif keyp == 104:
+            self.parent.OnVolumeUp(event=None)
+        elif keyp == 105:
+            self.parent.OnVolumeDown(event=None)
+        elif keyp == 106:
+            self.parent.OnMuteClick(event=None)
+        elif keyp == 107:
+            self.parent.OnRandomClick(event=None)
+        elif keyp == 108:
+            self.parent.OnRepeatClick(event=None)
             
     def GetSavedHotkeys(self):
         #grabbed the saved keys from the settings db, or not
@@ -51,40 +61,83 @@ class GlobalHotkeys():
         self.FILEDB = system_files.GetDirectories(self).DatabaseLocation()
         # hotkey-modifier, hotkey-previous, hotkey-play, hotkey-next, hotkey-stop
         hotkey_modifier = options_window.GetSetting('hotkey-modifier', self.FILEDB)
-        hotkey_previous = options_window.GetSetting('hotkey-previous', self.FILEDB)
-        hotkey_play = options_window.GetSetting('hotkey-play', self.FILEDB)
-        hotkey_next = options_window.GetSetting('hotkey-next', self.FILEDB)
-        hotkey_stop = options_window.GetSetting('hotkey-stop', self.FILEDB)
+        if hotkey_modifier == '0':
+            if os.name == 'nt':
+                hotkey_modifier = win32con.MOD_CONTROL
+            else:
+                hotkey_modifier = wx.MOD_CONTROL
+        if hotkey_modifier == '1':
+            if os.name == 'nt':
+                hotkey_modifier = win32con.MOD_SHIFT
+            else:
+                hotkey_modifier = wx.MOD_SHIFT
+        if hotkey_modifier == '2':
+            if os.name == 'nt':
+                hotkey_modifier = win32con.MOD_WIN
+            else:
+                hotkey_modifier = wx.MOD_WIN
+                 
+        h1 = options_window.GetSetting('hotkey-previous', self.FILEDB)
+        h2 = options_window.GetSetting('hotkey-play', self.FILEDB)
+        h3 = options_window.GetSetting('hotkey-next', self.FILEDB)
+        h4 = options_window.GetSetting('hotkey-stop', self.FILEDB)
+        h5 = options_window.GetSetting('hotkey-volume-up', self.FILEDB)
+        h6 = options_window.GetSetting('hotkey-volume-down', self.FILEDB)
+        h7 = options_window.GetSetting('hotkey-volume-mute', self.FILEDB)
+        h8 = options_window.GetSetting('hotkey-shuffle', self.FILEDB)
+        h9 = options_window.GetSetting('hotkey-repeat', self.FILEDB)
         
-        if (hotkey_modifier == False) & (hotkey_play == False):
+        if (hotkey_modifier == False) & (h1 == False):
             #use defaults
             if os.name == 'nt':
                 modc = win32con.MOD_CONTROL
                 h1 = win32con.VK_F1
                 h2 = win32con.VK_F2
                 h3 = win32con.VK_F3
-                h4 = win32con.VK_F4                
+                h4 = win32con.VK_F4
+                h5 = win32con.VK_F5
+                h6 = win32con.VK_F6
+                h7 = win32con.VK_F7
+                h8 = win32con.VK_F9
+                h9 = win32con.VK_F10
             else:
                 modc = wx.MOD_CONTROL
                 h1 = wx.WXK_F1
                 h2 = wx.WXK_F2
                 h3 = wx.WXK_F3
-                h4 = wx.WXK_F4                
+                h4 = wx.WXK_F4
+                h5 = wx.WXK_F5
+                h6 = wx.WXK_F6
+                h7 = wx.WXK_F7
+                h8 = wx.WXK_F9
+                h9 = wx.WXK_F10
                 
-            self.RegisterHotkeys(str(modc), h1, h2, h3, h4)
+            self.RegisterHotkeys(str(modc), h1, h2, h3, h4, h5, h6, h7, h8, h9)
         else:
             #use stored values
-            self.RegisterHotkeys(hotkey_modifier, hotkey_previous, hotkey_play, hotkey_stop, hotkey_next)
+            self.RegisterHotkeys(hotkey_modifier, h1, h2, h3, h4, h5, h6, h7, h8, h9)
         
-    def RegisterHotkeys(self, modc, h1, h2, h3, h4):
+    def RegisterHotkeys(self, modc, h1, h2, h3, h4, h5, h6, h7, h8, h9):
         #assign the keys
-        if (modc == False) | (modc == 0):
-            self.parent.RegisterHotKey(100, int(h1))
-            self.parent.RegisterHotKey(101, int(h2))
-            self.parent.RegisterHotKey(102, int(h3))
-            self.parent.RegisterHotKey(103, int(h4))
-        else:
-            self.parent.RegisterHotKey(100, eval(modc), int(h1))
-            self.parent.RegisterHotKey(101, eval(modc), int(h2))
-            self.parent.RegisterHotKey(102, eval(modc), int(h3))
-            self.parent.RegisterHotKey(103, eval(modc), int(h4))
+        mod_type = options_window.GetSetting('hotkey-modifier-type', self.FILEDB)
+        if (mod_type == '1'):
+            self.parent.RegisterHotKey(100, 0, int(h1))
+            self.parent.RegisterHotKey(101, 0, int(h2))
+            self.parent.RegisterHotKey(102, 0, int(h3))
+            self.parent.RegisterHotKey(103, 0, int(h4))
+            self.parent.RegisterHotKey(104, 0, int(h5))
+            self.parent.RegisterHotKey(105, 0, int(h6))
+            self.parent.RegisterHotKey(106, 0, int(h7))
+            self.parent.RegisterHotKey(107, 0, int(h8))
+            self.parent.RegisterHotKey(108, 0, int(h9))            
+        elif mod_type == '0':
+            self.parent.RegisterHotKey(100, int(modc), int(h1))
+            self.parent.RegisterHotKey(101, int(modc), int(h2))
+            self.parent.RegisterHotKey(102, int(modc), int(h3))
+            self.parent.RegisterHotKey(103, int(modc), int(h4))
+            self.parent.RegisterHotKey(104, int(modc), int(h5))
+            self.parent.RegisterHotKey(105, int(modc), int(h6))
+            self.parent.RegisterHotKey(106, int(modc), int(h7))
+            self.parent.RegisterHotKey(107, int(modc), int(h8))
+            self.parent.RegisterHotKey(108, int(modc), int(h9))
+            
