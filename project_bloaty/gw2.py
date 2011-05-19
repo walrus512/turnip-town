e0e5e414d4732cf4f3ac1dd0b2607306d395f5f3
11y3y3y3y43@gmail.com <11y3y3y3y43@gmail.com@072821e6-c1ea-11de-bbac-836dcc338a7a>
2011-05-19 12:49:11 +0000

    Project Bloaty

---
+++ project_bloaty/gw2.py
@@ -0,0 +1,48 @@
+#gw2.py
+
+import xmlrpclib
+import socket
+from main_server import xmlrpc_server
+
+from main_objects import playlist
+
+import threading
+
+class ServerThread(threading.Thread):
+    def __init__(self):
+        self.server = xmlrpc_server.Server()
+        threading.Thread.__init__(self)
+ 
+    def run(self):
+        self.server.Start()
+
+
+#p = subprocess.Popen('./main_server/xmlrpc_server.py')
+#x = xmlrpc_server.Server()
+#x.Start()
+#xmlrpc server thread
+server_thread = ServerThread()
+server_thread.start()
+
+hostname = socket.gethostname()
+s = xmlrpclib.ServerProxy('http://' + hostname + ':7777')
+#print s.pow(2,3)
+
+# Print list of available methods
+print s.system.listMethods()
+
+x = playlist.Playlist()
+print x.GetName()
+x.AddSong({'artist':'Beck', 'title':'Sad Song'})
+x.AddSong({'artist':'U2', 'title':'Gloria'})
+print x.playlist
+x.SetSongAttrib(0, 'rating', 0)
+print x.playlist
+print x.DeleteSong(1)
+print x.playlist
+x.InsertSong({'artist':'U2', 'title':'Lemon'}, 0)
+print x.playlist
+x.MoveSong(0, 1)
+print x.playlist
+print x.GetCount()
+ 
---
+++ project_bloaty/main_server/xmlrpc_server.py
@@ -0,0 +1,52 @@
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import socket
+
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+# Restrict to a particular path.
+    rpc_paths = ('/RPC2',)
+
+class Server:
+    """It's a server"""
+    #----------------------------------------------------------------------
+    def __init__(self, port=7777):
+    
+        # Create server
+        hostname = socket.gethostname()
+        self.server = SimpleXMLRPCServer((hostname, port),
+                                    requestHandler=RequestHandler)
+        self.server.register_introspection_functions()
+        
+        self.server.register_function(self.StopServer, 'shutdown')
+
+    # Register pow() function; this will use the value of
+    # pow.__name__ as the name, which is just 'pow'.
+    #server.register_function(pow)
+
+    # Register a function under a different name
+    #def adder_function(x,y):
+        #return x + y
+    
+
+    # Register an instance; all the methods of the instance are
+    # published as XML-RPC methods (in this case, just 'div').
+   # class MyFuncs:
+     #   def div(self, x, y):
+      #      return x // y
+
+    #server.register_instance(MyFuncs())
+    def Start(self):
+        # Run the server's main loop
+        print 'start'
+        self.server.serve_forever()
+        
+    def StopServer(self):
+        # Run the server's main loop
+        print 'stop'
+        self.server.shutdown()
+
+if __name__ == "__main__":
+    x = Server()
+    x.Start()
+    x.Stop()
